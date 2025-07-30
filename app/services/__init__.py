11fed8ba6879946c26e1342bafc537332037068f
Julio Macedo <juliocesar1076@gmail.com>
2025-07-30 10:21:23 -0300

    feat: export service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,13 +1,8 @@
 # app/services/__init__.py
 
-# Importações para facilitar o uso (como index.js no Node)
-from .frame_extractor import FrameExtractorService
-from .frame_search import FrameSearchService
 from .image_search_service import ImageSearchService
 
-# Exportar tudo que pode ser usado externamente
+# Export all available services
 __all__ = [
-    "FrameExtractorService",
-    "FrameSearchService",
     "ImageSearchService"
 ]
